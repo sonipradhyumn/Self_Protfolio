f9d12be5c8007405fb32a5515ed619acecc4bcc1
“pradhyumnsoniazilen” <pradhyumn.soni@azilen.com>
2022-01-13 02:19:05 +0530

    all new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
